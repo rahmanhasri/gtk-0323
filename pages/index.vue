02297d39cfffe0d32659da1aba53ec69a4983e6a
Rahman Hasri <rahman.hasri@gmail.com>
2022-11-03 20:46:27 +0700

    remove comment

--- pages/index.vue
+++ pages/index.vue
@@ -10,39 +10,10 @@
 </template>
 
 <script>
-// import 'leaflet/dist/leaflet.css'
-// import { LMap } from 'vue2-leaflet-nuxt';
-
 export default {
   name: 'IndexPage',
   components: {
-    // LMap,
   },
-  // mounted() {
-  //   const map = L.map("map-wrap", {
-  //     attributionControl: false,
-  //     zoomControl: false
-  //   }).setView(L.latLng(-37.79, 175.27), 12);
-  //   L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-  //     detectRetina: true,
-  //     maxNativeZoom: 17
-  //   }).addTo(map);
-
-  //   const pruneCluster = new this.PruneClusterForLeaflet();
-  //   const marker = new this.PruneCluster.Marker(59.8717, 11.1909);
-  //   pruneCluster.RegisterMarker(marker);
-  //   map.addLayer(pruneCluster);
-  // },
-  // data () {
-  //   return {
-  //     url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-  //     attribution:
-  //       '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-  //     zoom: 3,
-  //     center: [-7.5782556	, 109.2058436],
-  //     bounds: null
-  //   };
-  // },
   methods: {
     zoomUpdated (zoom) {
       this.zoom = zoom;
